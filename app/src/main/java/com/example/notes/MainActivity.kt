b425c25e5a2acf446c57b77f902323e1d3af1762
Prem Kumar <erprem013@gmail.com>
2024-03-25 01:43:26 +0530

    add new task to firebase done

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.example.notes
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.notes.databinding.ActivityMainBinding
+import com.example.notes.task.AddTask
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.addTask.setOnClickListener {
+            // Handle FAB click
+            val intent = Intent(this, AddTask::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/notes/task/AddTask.kt
+++ app/src/main/java/com/example/notes/task/AddTask.kt
@@ -1,21 +1,50 @@
 package com.example.notes.task
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.notes.MainActivity
 import com.example.notes.R
+import com.example.notes.databinding.ActivityAddTaskBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class AddTask : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddTaskBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var firebaseDatabase: FirebaseDatabase
+    private lateinit var databaseReference: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_add_task)
+        binding = ActivityAddTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Initialize Firebase Auth
+        firebaseAuth = FirebaseAuth.getInstance()
+        val currentUser = firebaseAuth.currentUser
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        databaseReference = firebaseDatabase.getReference("Users").child(currentUser?.uid.toString())
+
+        // Set up the save button
+        binding.done.setOnClickListener {
+            val title = binding.taskTitle.text.toString()
+            val description = binding.taskDescription.text.toString()
+            val timeStamp = System.currentTimeMillis()
+            val task = TaskDataClass(title, description, timeStamp)
+            databaseReference.child("Tasks").push().setValue(task)
+            finish()
         }
     }
 }
